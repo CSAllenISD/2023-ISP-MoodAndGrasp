c411dd7c08ab6263d4b9b0fb6f7d551a155fdd0c
KaiosKen <robel.abraham@student.allenisd.org>
2023-02-19 20:31:37 -0600

    Created an empty Profile page #45
    The page can be reached through the Navigation Bar, and shows the username of the user who logged in. Can only be seen on the Navigation bar if logged in.

--- users/views.py
+++ users/views.py
@@ -14,4 +14,5 @@ def register(request):
 	else:
 		form = UserRegisterForm()
 	return render(request, 'users/register.html', {'form':form})
-
+def profile(request):
+	return render(request, 'users/profile.html')
