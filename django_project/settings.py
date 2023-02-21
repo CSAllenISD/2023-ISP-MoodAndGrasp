b0fae642203624181985eccee0cb187115ddc77a
KaiosKen <robel.abraham@student.allenisd.org>
2023-02-21 11:27:46 -0600

    More Progress on #45, and progress towards issue #48
    A Couple of new features have been added. The User profile is now shown on the profile page, along with Username and Email. No option to customize or choose it yet, so a default profile is automatically made when a user registers and gives it a profile of default.png.

--- django_project/settings.py
+++ django_project/settings.py
@@ -122,6 +122,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+
 STATICFILES_DIRS = ('/Users/robelabraham/bootstrap',)
 
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
--- django_project/urls.py
+++ django_project/urls.py
@@ -17,6 +17,8 @@ from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
 from users import views as user_views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -28,3 +30,6 @@ urlpatterns = [
 
 
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
