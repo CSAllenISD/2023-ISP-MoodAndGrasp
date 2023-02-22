505eb9d52ce0a283f8f7af6f7d500ae8190ee508
KaiosKen <robel.abraham@student.allenisd.org>
2023-02-21 21:12:30 -0600

    User Profile Customization Skeleton #48
    Now, users can customize their profile in the profile webpage, and can change their username, email, and photo. So far no password reset has been made but it is on the way

--- users/signals.py
+++ users/signals.py
@@ -7,6 +7,7 @@ from .models import Profile
 def create_profile(sender, instance, created, **kwargs):
 	if created:
 		Profile.objects.create(user=instance)
+		
 
 @receiver(post_save, sender=User)
 def save_profile(sender, instance, **kwargs):
