ca91a562e193eb6e95805d0af0efcb17ccd29ff7
KaiosKen <robel.abraham@student.allenisd.org>
2023-05-11 23:32:57 -0500

    Attempting to fix errors

--- django_env/bin/activate.fish
+++ django_env/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/nahom/2023-ISP-MoodAndGrasp/django_env"
+set -gx VIRTUAL_ENV "/Users/robelabraham/Project/django_env"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
